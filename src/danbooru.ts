fb217391298d51ee4ed2b075f98104b6eadb3cc0
honeygooner <206956665+honeygooner@users.noreply.github.com>
2026-01-15 00:16:14 -0500

    refactor: validate url params for danbooru requests

--- src/danbooru.ts
+++ src/danbooru.ts
@@ -1,23 +1,33 @@
 import { HttpClient, HttpClientRequest, HttpClientResponse, UrlParams } from "@effect/platform";
 import { NodeHttpClient } from "@effect/platform-node";
-import { Array, Chunk, Data, Effect, Function, Option, Schedule, Schema, Stream } from "effect";
-import { USER_AGENT } from "./util.ts";
+import { Chunk, Data, Effect, Function, Option, Schedule, Schema, Stream } from "effect";
+import pkg from "../package.json" with { type: "json" };
 
-class DanbooruError extends Data.TaggedError("DanbooruError") {
-  constructor(fields: typeof DanbooruError.ResponseSchema.Type) {
-    super();
-    this.name = fields.error || super.name;
-    this.message = fields.message;
-    this.stack = fields.backtrace?.join("\n");
-  }
+/** @see {@link https://github.com/danbooru/danbooru/blob/757a709/app/controllers/application_controller.rb#L172 | danbooru/app/controllers/application_controller.rb#L172} */
+class DanbooruError extends Schema.Class<DanbooruError>("DanbooruError")({
+  success: Schema.Literal(false),
+  message: Schema.String,
+  error: Schema.NullOr(Schema.String),
+  backtrace: Schema.NullOr(Schema.Array(Schema.String)),
+}) {}
 
-  static readonly ResponseSchema = Schema.Struct({
-    success: Schema.Literal(false),
-    message: Schema.String,
-    error: Schema.NullOr(Schema.String),
-    backtrace: Schema.NullOr(Schema.Array(Schema.String)),
-  });
-}
+/** @see {@link https://danbooru.donmai.us/wiki_pages/help:common_url_parameters | Danbooru Wiki | help:common url parameters} */
+class DanbooruParams extends Schema.Class<DanbooruParams>("DanbooruParams")({
+  /** @see {@link https://danbooru.donmai.us/wiki_pages/help:common_url_parameters#dtext-limit | Danbooru Wiki | help:common url parameters} */
+  limit: Schema.optional(Schema.NumberFromString),
+  /** @see {@link https://danbooru.donmai.us/wiki_pages/help:common_url_parameters#dtext-page | Danbooru Wiki | help:common url parameters} */
+  page: Schema.optional(Schema.Union(Schema.String, Schema.NumberFromString)),
+}) {}
+
+/** @see {@link https://github.com/danbooru/danbooru/blob/757a709/db/structure.sql#L253-L260 | danbooru/db/structure.sql#L253-L260} */
+class ArtistUrl extends Schema.Class<ArtistUrl>("ArtistUrl")({
+  id: Schema.Number,
+  artist_id: Schema.Number,
+  url: Schema.String,
+  created_at: Schema.Date,
+  updated_at: Schema.Date,
+  is_active: Schema.Boolean,
+}) {}
 
 class DanbooruClient extends Effect.Service<DanbooruClient>()("DanbooruClient", {
   dependencies: [NodeHttpClient.layer],
@@ -29,8 +39,15 @@ class DanbooruClient extends Effect.Service<DanbooruClient>()("DanbooruClient", 
           Function.flow(
             HttpClientRequest.prependUrl(baseUrl),
             HttpClientRequest.setHeader("Accept", "application/json"),
-            HttpClientRequest.setHeader("User-Agent", USER_AGENT),
+            HttpClientRequest.setHeader("User-Agent", `${pkg.name}/${pkg.version} (${pkg.repository.url})`), // prettier-ignore
             HttpClientRequest.setUrlParam("format", "json"),
+          ),
+        ),
+        HttpClient.mapRequestEffect((request) =>
+          Function.pipe(
+            request.urlParams,
+            UrlParams.schemaStruct(DanbooruParams),
+            Effect.as(request),
           ),
         ),
         HttpClient.filterStatusOk,
@@ -38,49 +55,34 @@ class DanbooruClient extends Effect.Service<DanbooruClient>()("DanbooruClient", 
           schedule: Schedule.exponential("125 millis"),
           times: 5,
         }),
-        HttpClient.catchTag("ResponseError", (error) =>
+        HttpClient.catchTag("ResponseError", (responseError) =>
           Function.pipe(
-            error.response.json,
-            Effect.flatMap(Schema.decodeUnknown(DanbooruError.ResponseSchema)),
-            Effect.flatMap((fields) => new DanbooruError(fields)),
+            responseError.response.json,
+            Effect.flatMap(Schema.decodeUnknown(DanbooruError)),
+            Effect.flatMap((danbooruError) => new DanbooruClientError(danbooruError)),
           ),
         ),
       ),
     ),
 }) {}
 
+class DanbooruClientError extends Data.TaggedError("DanbooruClientError")<DanbooruError> {}
+
 export const Danbooru = DanbooruClient.Default("https://danbooru.donmai.us");
 export const Testbooru = DanbooruClient.Default("https://testbooru.donmai.us");
 
-class ArtistUrl extends Schema.Class<ArtistUrl>("ArtistUrl")({
-  id: Schema.Number,
-  artist_id: Schema.Number,
-  url: Schema.String,
-  created_at: Schema.Date,
-  updated_at: Schema.Date,
-  is_active: Schema.Boolean,
-}) {}
-
-export const getArtistUrls = (urlParams?: CommonUrlParameters) =>
-  Effect.flatMap(
+export function getArtistUrls(urlParams?: DanbooruParams & UrlParams.CoercibleRecord) {
+  return Effect.flatMap(
     DanbooruClient.use((client) => client.get("/artist_urls", { urlParams })),
     HttpClientResponse.schemaBodyJson(Schema.Array(ArtistUrl)),
   );
+}
 
-export const getArtistUrlsStream = (urlParams?: CommonUrlParameters) =>
-  Stream.paginateChunkEffect(urlParams?.page, (page) =>
+export function getArtistUrlsStream(urlParams?: DanbooruParams & UrlParams.CoercibleRecord) {
+  return Stream.paginateChunkEffect(urlParams?.page, (page) =>
     Effect.map(getArtistUrls({ ...urlParams, page }), (artistUrls) => [
       Chunk.fromIterable(artistUrls),
-      Option.map(Array.last(artistUrls), ({ id }) => `b${id}` as const),
+      Option.map(Option.fromNullable(artistUrls.at(-1)), ({ id }) => `b${id}`),
     ]),
   );
-
-/** @see {@link https://danbooru.donmai.us/wiki_pages/help:common_url_parameters | Danbooru Wiki | help:common url parameters} */
-type CommonUrlParameters = {
-  /** @see {@link https://danbooru.donmai.us/wiki_pages/help:common_url_parameters#dtext-limit | Danbooru Wiki | help:common url parameters} */
-  readonly limit?: number;
-  /** @see {@link https://danbooru.donmai.us/wiki_pages/help:common_url_parameters#dtext-page | Danbooru Wiki | help:common url parameters} */
-  readonly page?: number | `a${number}` | `b${number}`;
-  /** @see {@link https://danbooru.donmai.us/wiki_pages/help:common_url_parameters#dtext-search | Danbooru Wiki | help:common url parameters} */
-  readonly search?: UrlParams.CoercibleRecord;
-};
+}
--- src/util.ts
+++
@@ -1,3 +0,0 @@
-import pkg from "../package.json" with { type: "json" };
-
-export const USER_AGENT = `${pkg.name}/${pkg.version} (${pkg.repository.url})`;
